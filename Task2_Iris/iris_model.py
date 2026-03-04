f94026a60b63980235e80f8826e549cf6c221bc5
Dhana-gitd <nithyabarri@gmail.com>
2026-03-04 16:51:22 +0530

    Completed Iris classification model

---
+++ Task2_Iris/iris_model.py
@@ -0,0 +1,32 @@
+# Import libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+df = pd.read_csv("iris_dataset.csv")
+
+print("First 5 rows:")
+print(df.head())
+
+# Features and target
+X = df.drop("species", axis=1)
+y = df["species"]
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Model
+model = LogisticRegression()
+
+# Train
+model.fit(X_train, y_train)
+
+# Predict
+y_pred = model.predict(X_test)
+
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Model Accuracy:", accuracy)
